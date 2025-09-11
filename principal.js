ecd7065c64ee644b4a6490cc33415d499509c530
Fernandazinha <fernanda.rezende.silva16@escola.pr.gov.br>
2025-09-11 08:51:58 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,19 +1,21 @@
 //muda o nome a partir do seletor de classe
-var subtitulo = document.querySelector("subtitulo")
-var titulo = document.querySelector("titulo");
+var subtitulo = document.querySelector(".subtitulo")
+var titulo = document.querySelector(".titulo");
 titulo.textContent = "Fernanda nutrição";
-subtitulo.textContent = "Fernanda Nutrição";
+subtitulo.textContent = "Meus clientes";
 
 //acessar a tag tr - paciente Paulo
-var paciente = document.querySelector(".paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-for(var i =0; i < paciente.length; i++){
+for(var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
+
 //seleciona o conteúdo da tag
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
 //seleciona o conteúdo altuta tag
-var tdAltura = paciente.querySelector(".info-altura") 
+var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
  
 //calcula imc
@@ -24,20 +26,19 @@ var pesoValido = true;
 var alturaValida = true; 
 
 if(pesoValido && alturaValida){
-    //acessa e altura po imc
-    var tdImc = paciente.querySelector("info-imc");
+    //acessa e altura o imc
+    var tdImc = paciente.querySelector(".info-imc");
     tdImc.textContent = imc;
 }
 
-//define limites e peso e altura
+//define limites de peso e altura
 if(peso <= 0 || peso >= 1000){
     alert("Peso inválido");
     var pesoValido = false;
-    tdImc.textContent = "Peso inválida"
+    tdImc.textContent = "Peso inválido!"
 }
 if(altura <= 0 || altura >= 3.00){
-    console.log("Altura invália");
-    alert("Altura inválido");
+    alert("Altura inválida");
     var alturaValida = false;
     tdImc.textContent = "Altura Inválida";
 }
